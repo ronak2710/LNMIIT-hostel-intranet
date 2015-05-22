62966b1f44d0374bea40c32cd4a4d5913a91c832
Arush <freeza@example.com>
2015-05-23 00:24:27 +0530

    initial project version

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>LNMIIT Intranet Portal</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+<!--
+.style1 {
+	font-size: 50px
+}
+.style4 { 
+         font-size: 46px;
+         font-family: Georgia, "Times New Roman", Times, serif;
+       }
+-->
+</style>
+
+<link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+
+</head>
+
+<body>
+<div class="body_header_1">
+  <div class="header">
+    <div class="header">
+      <table width="1002" height="150" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+        <td width="693"><div align="center" class="style1">
+            <div align="left" class="style4">
+              <p><a href="index.php"><span style="color:#684c00;"><u>LNMIIT Intranet Portal</u></span></a></p>
+            </div>
+          </div></td>
+          <td width="309" height="150"><div align="center"><a href="http://www.lnmiit.ac.in" target="_blank"><img src="images/logo.png" width="218" height="106"/></a></div></td>
+          
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
+
